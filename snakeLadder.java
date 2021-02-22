adfd223973fda11e9a08573e2c78ad59bc4ff50c
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-22 20:05:32 +0530

    Added: Player gets Exact Winning Position 100

--- snakeLadder.java
+++ snakeLadder.java
@@ -10,30 +10,43 @@ public class snakeLadder {
 		int currentPosition = 0;
 
 		//Computation
-		while (currentPosition != END_POSITION && currentPosition < END_POSITION)
+		while (currentPosition != END_POSITION)
 		{
-			int rollDice;
+			int rollDice, rollDice1;
 			rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
 			currentPosition += rollDice;
 
-			int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
-			switch (checkPlay)
-			{
-				case LADDER:
-					rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-					currentPosition += rollDice;
-					break;
-				case SNAKE:
-					rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-					if (currentPosition < rollDice) {
-						currentPosition = START_POSITION;
-					}else {
-						currentPosition -= rollDice;
-					}
-					break;
-				default:
-					//No Play
-					currentPosition += 0;
+			if (currentPosition > END_POSITION) {
+				currentPosition -= rollDice;
+			}else {
+
+				int checkPlay = (int) Math.floor(Math.random() * 10) % 3;
+				switch (checkPlay)
+				{
+					case LADDER:
+						rollDice1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
+						if ((END_POSITION - currentPosition) < rollDice1) {
+							currentPosition += 0;
+						}else {
+							currentPosition += rollDice1;
+						}
+						break;
+					case SNAKE:
+						rollDice1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
+						if (currentPosition < rollDice1) {
+							currentPosition = START_POSITION;
+						}else {
+							currentPosition -= rollDice1;
+						}
+						break;
+					default:
+						//No Play
+						if (currentPosition > END_POSITION) {
+							currentPosition -= rollDice;
+						}else {
+							currentPosition += 0;
+						}
+				}
 			}
 			System.out.println("Current Position : "+currentPosition);
 		}
