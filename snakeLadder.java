97858b68a313b6eaf7614d4d20224ab094043f4b
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-23 14:10:10 +0530

    Refactor: For Win and Total Die Number for Single Player

--- snakeLadder.java
+++ snakeLadder.java
@@ -1,3 +1,4 @@
+
 public class snakeLadder {
 
 	public static final int START_POSITION = 0;
@@ -6,22 +7,28 @@ public class snakeLadder {
 	public static final int SNAKE = 1;
 
 	public static void main (String[] args) {
+
 		//Variables
 		int currentPosition = 0;
 		int dieTimes = 0;
+		int player = 1;
+
+		System.out.println( "Player" + player + " is at Starting Position :" +START_POSITION );
 
 		//Computation
 		while (currentPosition != END_POSITION)
 		{
+
 			int rollDice, rollDice1;
 			rollDice = (int) Math.floor(Math.random() * 10) % 6 + 1;
 			currentPosition += rollDice;
 			dieTimes = dieTimes + 1;
 
-			System.out.println("Die Number : "+dieTimes);
-
 			if (currentPosition > END_POSITION) {
 				currentPosition -= rollDice;
+				System.out.println("Current Position : "+currentPosition);
+			}else if (currentPosition == END_POSITION){
+				//Game over
 				System.out.println("Current Position : "+currentPosition);
 			}else {
 				System.out.println("Current Position : "+currentPosition);
@@ -36,26 +43,29 @@ public class snakeLadder {
 						}else {
 							currentPosition += rollDice1;
 						}
-						System.out.println("Die Number : "+dieTimes);
+						System.out.println("Ladder is there");
 						System.out.println("Current Position : "+currentPosition);
 						break;
 					case SNAKE:
 						rollDice1 = (int) Math.floor(Math.random() * 10) % 6 + 1;
 						dieTimes = dieTimes + 1;
-						if (currentPosition < rollDice1) {
+						if (currentPosition <= rollDice1) {
 							currentPosition = START_POSITION;
+							System.out.println("Game Restart");
 						}else {
 							currentPosition -= rollDice1;
 						}
-						System.out.println("Die Number : "+dieTimes);
+						System.out.println("Sanke is there");
 						System.out.println("Current Position : "+currentPosition);
 						break;
 					default:
 						//No Play
-							currentPosition += 0;
+						System.out.println("No Play is there");
+						currentPosition += 0;
 				}
 			}
 		}
 		System.out.println("Number of Times Die Played : "+dieTimes);
+		System.out.println("Player" + player + " Win the Game");
 	}
 }
